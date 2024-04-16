c0b34b43ffb1b6ef0f0a1ac757628df5b331483e
rthomas86 <rebkthomas72@gmail.com>
2024-04-15 18:25:40 -0700

    Starting the questions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
+var body = document.body;
 var startButton = document.querySelector(".btn_start");
 var instructions = document.querySelector(".instructions");
+var questions = document.querySelector(".questions");
+
+var question = document.createElement("h2");
+var buttonOne = document.createElement("button");
+buttonOne.id = "1";
+buttonOne.style = "border-radius:8px; background-color:blueviolet; color:white";
+buttonOne.textContent = "Button One";
 
 startButton.addEventListener("click", startQuiz);
-
+buttonOne.addEventListener("click", questionResultOne);
+buttonOne.addEventListener("mouseover", () => {
+  buttonOne.style.backgroundColor = "palevioletred";
+});
+buttonOne.addEventListener("mouseout", () => {
+  buttonOne.style.backgroundColor = "blueviolet";
+});
 function startQuiz() {
   instructions.innerHTML = "";
+  question.textContent = "This is question1!";
+  questions.appendChild(question);
+
+  questions.appendChild(buttonOne);
 }
+
+function questionResultOne() {
+  alert(this.id);
+}
+
+function clearQuestions() {
+  questions.innerHTML = "";
+}
+
+clearQuestions();
